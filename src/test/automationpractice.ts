cc8daf161760c853894555ff8dcc9cb084f49f60
lorblak <kvasquez@fi.uba.ar>
2022-02-12 13:37:46 -0300

    refactor(test/automationpractice.ts): selectors were replaced by pom file

--- src/test/automationpractice.ts
+++ src/test/automationpractice.ts
@@ -1,29 +1,32 @@
 import { Selector, t } from "testcafe";
+import HomePage from "../pages/automationpractice/home.page";
+import LoginPage from '../pages/automationpractice/login.page';
+import RegisterPage from '../pages/automationpractice/register.page';
 
 fixture `Automation Practice`
     .page `http://automationpractice.com/index.php`;
 
 test('Go to sign in page and get register account error', async t => {
     await t 
-        .click('.login')
-        .typeText('#email_create', 'test@test.com')
-        .click('#SubmitCreate')
-        .expect(Selector('#create_account_error').innerText).contains('An account using this email address has already been registered. Please enter a valid password or request a new one.')
+        .click(HomePage.btnLogin)
+        .typeText(LoginPage.inputCreateEmail, 'test@test.com')
+        .click(LoginPage.btnSubmitCreate)
+        .expect(LoginPage.alertCreateAccountError.innerText).contains('An account using this email address has already been registered. Please enter a valid password or request a new one.')
 });
 
 test('Go to register account page', async t => {
     await t 
-        .click('.login')
-        .typeText('#email_create', 'karma@test.com')
-        .click('#SubmitCreate')
-        .expect(Selector('h3[class="page-subheading"]').innerText).contains('YOUR PERSONAL INFORMATION')
+        .click(HomePage.btnLogin)
+        .typeText(LoginPage.inputCreateEmail, 'karma@test.com')
+        .click(LoginPage.btnSubmitCreate)
+        .expect(RegisterPage.titleFormPage.innerText).contains('YOUR PERSONAL INFORMATION')
 });
 
 test('Go to sign in page and get autentication error', async t => {
     await t
-        .click('.login')
-        .typeText('#email', 'test@test.com')
-        .typeText('#passwd', 'password123')
-        .click('#SubmitLogin')
-        .expect(Selector('div[class="alert alert-danger"]').innerText).contains('Authentication failed.')
+        .click(HomePage.btnLogin)
+        .typeText(LoginPage.inputEmail, 'test@test.com')
+        .typeText(LoginPage.inputPassword, 'password123')
+        .click(LoginPage.btnSubmitLogin)
+        .expect(LoginPage.alertLoginError.innerText).contains('Authentication failed.')
 });
